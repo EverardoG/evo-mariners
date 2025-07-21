a69dc63ac9a6b0e01c207502637641af331f96c0
EverardoG <everardo.a.gonzalez@gmail.com>
2025-07-21 15:00:30 -0400

    add compatibility with lima on macos

---
+++ scripts/start-lima-dev.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+INSTANCE=ubuntu
+PROJECT_PATH="$HOME/evo-mariners"
+
+# echo "Starting Lima instance '$INSTANCE'..."
+limactl start "$INSTANCE"
+
+echo "Lima VM started."
+
+echo "Launching VS Code connected to '$INSTANCE'..."
+code --remote ssh-remote+lima-$INSTANCE $PROJECT_PATH
