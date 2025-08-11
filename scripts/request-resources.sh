892831d6cdf8fce6e639d58e36dd037e710b6f86
EverardoG <everardo.a.gonzalez@gmail.com>
2025-08-11 13:55:43 -0700

    add script for requesting hpc resources allocation

---
+++ scripts/request-resources.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Run this locally to request resources on the hpc for running a vscode server
+
+ssh -t submit-a \
+    "salloc -A kt-lab --partition=preempt --time=01:00:00 -c 12 --nodes=1 --mem=16G --nodelist=cn-v-[1-9]"
