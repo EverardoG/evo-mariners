93b8b8d2c0ba7772d033ebd699152636541b3a95
EverardoG <everardo.a.gonzalez@gmail.com>
2025-08-31 10:46:08 -0700

    checkpoint; trying to figure out logging queue

--- scripts/one-off/central_log.py
+++ scripts/one-off/central_log.py
@@ -3,9 +3,39 @@ import logging.handlers
 import multiprocessing
 import os
 
-def worker(msg):
-    logger = logging.getLogger("worker")
-    logger.info(f"Subprocess log from PID {os.getpid()}: {msg}")
+class CentralLoggerMain:
+    def __init__(self, log_path="main.log", num_workers=2):
+        self.log_queue = multiprocessing.Queue()
+        self.logger = logging.getLogger("main")
+        self.logger.setLevel(logging.INFO)
+        self.file_handler = logging.FileHandler(log_path)
+        formatter = logging.Formatter('%(asctime)s %(processName)s %(levelname)s: %(message)s')
+        self.file_handler.setFormatter(formatter)
+        self.queue_listener = logging.handlers.QueueListener(self.log_queue, self.file_handler)
+        self.num_workers = num_workers
+
+    @staticmethod
+    def worker(msg):
+        logger = logging.getLogger("worker")
+        logger.info(f"Subprocess log from PID {os.getpid()}: {msg}")
+
+    def run(self):
+        self.queue_listener.start()
+        self.logger.addHandler(self.file_handler)
+        self.logger.info("Main process started logging.")
+
+        pool = multiprocessing.Pool(
+            processes=self.num_workers,
+            initializer=worker_initializer,
+            initargs=(self.log_queue,)
+        )
+        for i in range(4):
+            pool.apply_async(CentralLoggerMain.worker, args=(f"message {i}",))
+        pool.close()
+        pool.join()
+
+        self.logger.info("Main process finished logging.")
+        self.queue_listener.stop()
 
 def worker_initializer(log_queue):
     logger = logging.getLogger("worker")
@@ -15,32 +45,5 @@ def worker_initializer(log_queue):
     handler = logging.handlers.QueueHandler(log_queue)
     logger.addHandler(handler)
 
-def main():
-    log_queue = multiprocessing.Queue()
-    logger = logging.getLogger("main")
-    logger.setLevel(logging.INFO)
-    file_handler = logging.FileHandler("main.log")
-    formatter = logging.Formatter('%(asctime)s %(processName)s %(levelname)s: %(message)s')
-    file_handler.setFormatter(formatter)
-    queue_listener = logging.handlers.QueueListener(log_queue, file_handler)
-    queue_listener.start()
-
-    logger.addHandler(file_handler)
-    logger.info("Main process started logging.")
-
-    # Use Pool with initializer
-    pool = multiprocessing.Pool(
-        processes=2,
-        initializer=worker_initializer,
-        initargs=(log_queue,)
-    )
-    for i in range(4):
-        pool.apply_async(worker, args=(f"message {i}",))
-    pool.close()
-    pool.join()
-
-    logger.info("Main process finished logging.")
-    queue_listener.stop()
-
 if __name__ == "__main__":
-    main()
+    CentralLoggerMain().run()
